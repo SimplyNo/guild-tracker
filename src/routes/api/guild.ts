12ffe5d4cb2d71c973da7e9a753ab0711915d503
simplyno <simplehno@gmail.com>
2024-06-03 22:19:52 +0000

    add optinos to /guild

--- src/routes/api/guild.ts
+++ src/routes/api/guild.ts
@@ -8,9 +8,15 @@ guildRouter.get('/api/guild/:id', async (req, res) => {
     const options = {
         type: <'player' | 'name' | 'id'>req.query.type || 'name',
         parseNames: <string>req.query.parseNames || false,
+        force: <string>req.query.force || false
     }
-    console.log('options:', options)
-    const guild = await Wrappers.hypixel.guild(req.params.id, options.type, { updateAPI: true, parseNames: new Boolean(options.parseNames).valueOf() }).catch((e: ({ error: string, message: string })) => e).then(e => e);
+    const opts = {
+        updateAPI: true,
+        parseNames: new Boolean(options.parseNames).valueOf(),
+        cache: !(new Boolean(options.force).valueOf())
+    };
+    console.log('opts:', opts)
+    const guild = await Wrappers.hypixel.guild(req.params.id, options.type, opts).catch((e: ({ error: string, message: string })) => e).then(e => e);
     if (!('_id' in guild)) return res.status(400).json({ error: guild.error || 'API Error', message: guild.message || 'No message provided' });
 
 
